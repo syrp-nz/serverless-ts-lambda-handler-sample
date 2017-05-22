dd3ba938ef46df80be98f1889b77642b30ad149e
Maxime Rainville <maxime@syrp.co.nz>
2017-05-22 16:54:54 +1200

    Create a login function.

---
+++ src/handlers/Login.ts
@@ -0,0 +1,67 @@
+import * as LambdaHandler from 'ts-lambda-handler';
+import * as JWT from 'jsonwebtoken';
+
+const secret = 'my_super_evil_secret';
+
+/**
+ * This function demonstrate a very simple authorization process. It expect to receive a JSON object with a `username`
+ * and `password` attribute. If the `username` and the `password` match, then the user is issued a Json Web Token (JWT)
+ */
+class LoginHandler extends LambdaHandler.Handlers.AbstractHandler {
+    public process(request: LambdaHandler.Request, response: LambdaHandler.Response): Promise<void> {
+        const data = this.request.getBodyAsJSON();
+
+        return this.validate(data).then(() => {
+            // Build a jwt for the current user
+            const jwt = this.signJwt(data.username);
+
+            // For simplicity, we'll store the JWT althought.
+
+            const cookieOptions: LambdaHandler.CookieOptions = {
+                maxAge: 5 * 60,     // We'll set the cookie expiration date to match the JWT's one.
+                httpOnly: false,
+            };
+
+            response
+                // For simplicity, store the JWT in a cookie. They are better ways of doing this
+                .addCookie('jwt', jwt , cookieOptions)
+                .setStatusCode(204)
+                .send();
+            return Promise.resolve();
+        })
+
+    }
+
+    protected validate(data: any): Promise<void> {
+        if (data && data.user && data.password) {
+            if (data.user == data.password) {
+                // We have a valid user.
+                return Promise.resolve();
+            } else {
+                // If the parameters are there, but don't match return a 401 error
+                return Promise.reject(new LambdaHandler.Errors.UnauthorizedError());
+            }
+        } else {
+            // If we are missing a parameter return a 400 error
+            return Promise.reject(new LambdaHandler.Errors.BadRequestError());
+        }
+    }
+
+    /**
+     * Create a JSON web token that is valid for 5 min. If you wanted to you could just issue a session ID or perform some other type of authorization.
+     */
+    protected signJwt(username:string): string {
+        // Build a payload
+        const payload = {
+            username: username
+        };
+
+        // Our token will be valid for 5 minutes
+        const options = {expiresIn: 5 * 60};
+
+        // Build the actual token
+        return JWT.sign(payload, secret, options)
+    }
+}
+
+export let handler = new LoginHandler().handle;
